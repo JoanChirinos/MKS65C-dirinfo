598098c37c72c4397719e44e70e4937fc522124c
JoanChirinos <jchirinos@stuy.edu>
2018-11-02 13:29:34 -0400

    fixed dir

--- dir.c
+++ dir.c
@@ -7,117 +7,117 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-//
-// void print_contents(char* dir) {
-//   DIR* d;
-//   d = opendir(dir);
-//   struct dirent* entry = readdir(d);
-//   while (entry) {
-//     int file_name = open(entry->d_name);
-//     char* type = (entry -> d_type == 8) ? "File: " : "Directory: ";
-//     printf("%s |", type);
-//     if (!strcmp(file_name, "File: ")) {
-//       struct stat * file_stats = calloc(sizeof(struct stat), 1);
-//       stat(file_name, file_stats);
-//       printf("%ld\n", file_stats->st_size);
-//     }
-//     printf("%d\n", file_name);
-//     entry = readdir(d);
-//   }
-//   closedir(d);
-// }
 
-void printHelper(int fileMode, int b, int c, char p) {
-  if ((fileMode & b) && (fileMode & c))
-  printf("%c",p);
-  else
-  printf("-");
+const char FILE_D_TYPE = 8;
+const char DIRECTORY_D_TYPE = 4;
+
+long long int size_of_dir(char* path) {
+  long long int total_size = 0;
+
+  DIR* current_directory = opendir(path);
+
+  struct dirent* current_file;
+  struct stat* current_file_stats = calloc(sizeof(struct stat), 1);
+  char whole_path[512];
+
+  while ((current_file = readdir(current_directory)) != NULL) {
+
+    // if current file is a directory
+    if (current_file->d_type == DIRECTORY_D_TYPE) {
+
+      // skip self and parent
+      if (!strcmp(current_file->d_name,".") || !strcmp(current_file->d_name,"..")) {
+        continue;
+      }
+
+      // cat dir path to whole_path
+      strcpy(whole_path, "");
+      strcat(whole_path, path);
+      strcat(whole_path, "/");
+      strcat(whole_path, current_file->d_name);
+
+      // run this fxn on new directory
+      total_size += size_of_dir(whole_path);
+    }
+
+    // if current file is a file
+    if (current_file->d_type == FILE_D_TYPE) {
+      // resets and copies path to whole_path + filename
+      strcpy(whole_path, "");
+      strcat(whole_path, path);
+      strcat(whole_path, "/");
+      strcat(whole_path, current_file->d_name);
+
+      // gets stats of current FILE
+      stat(whole_path, current_file_stats);
+
+      // adds current file size to total_size
+      total_size += current_file_stats->st_size;
+    }
+  }
+  free(current_file_stats);
+  return total_size;
 }
 
-void printPermissions(int fileMode) {
-  printf("-");
-  /* owner permissions */
-  printHelper(fileMode, S_IRUSR, S_IREAD, 'r');
-  printHelper(fileMode, S_IWUSR, S_IWRITE, 'w');
-  printHelper(fileMode, S_IXUSR, S_IEXEC, 'x');
-  /* group permissions */
-  printHelper(fileMode, S_IRGRP, S_IREAD, 'r');
-  printHelper(fileMode, S_IWGRP, S_IWRITE, 'w');
-  printHelper(fileMode, S_IXGRP, S_IEXEC, 'x');
-  /* other user permissions */
-  printHelper(fileMode, S_IROTH, S_IREAD, 'r');
-  printHelper(fileMode, S_IWOTH, S_IWRITE, 'w');
-  printHelper(fileMode, S_IXOTH, S_IEXEC, 'x');
-}
+void print_info(struct dirent* file) {
+  // get file stats
+  struct stat* file_stats = calloc(sizeof(struct stat), 1);
+  stat(file->d_name, file_stats);
 
-void statP(char * str) {
-  struct stat * pter = malloc(sizeof(struct stat)); // allocate some memory
-  stat(str, pter); // record metadata into pter
-  // printing file info:
-  long int size = pter -> st_size;
-  printPermissions(pter -> st_mode);
-  // printf("mode(permissions): (%3o) ", pter -> st_mode&0777);
-  if(size < 1000)
-  printf(" %ldB ", size);
-  else if (size < 1000000)
-  printf(" %ld.%ldKB ", size/1000, size%1000);
-  else if (size < 1000000000)
-  printf(" %ld.%ldMB ", size/1000000, size%1000000);
-  else
-  printf(" %ld.%ldGB ", size/1000000000, size%1000000000);
+  // mode
+  char* readable_mode = calloc(sizeof(char), 12);
+  // converts octal mode to what we see in ls -l
+  strmode(file_stats->st_mode, readable_mode);
+  // printing mode
+  printf("%s ", readable_mode);
+  // freeing so I don't forget later
+  free(readable_mode);
+
+  // number of links!
+  printf("%3d ", file_stats->st_nlink);
+
+  // user
+  printf("%3u ", file_stats->st_uid);
+
+  // group
+  printf("%3u ", file_stats->st_gid);
+
+  if (!strcmp(file->d_name, "..")) {
+    printf("   ? B\t");
+  }
+  else {
+    long long int size;
+    if (file->d_type == DIRECTORY_D_TYPE) {
+        size = size_of_dir(file->d_name);
+    }
+    if (file->d_type == FILE_D_TYPE) {
+      size = file_stats->st_size;
+    }
+    if (size >= 1000000000) {printf("%4lld G\t", size / 1000000000);}
+    else if (size >= 1000000) {printf("%4lld M\t", size / 1000000);}
+    else if (size >= 1000) {printf("%4lld K\t", size / 1000);}
+    else printf("%4lld B\t", size);
+  }
+
+  // time
   char temp[100];
-  strftime(temp, 100, "%b %d %R", localtime(&(pter -> st_atime)));
-  printf("%s ", temp);
-  printf("%s\n", str);
-  free(pter);
+  strftime(temp, 100, "%b %d %R", localtime(&(file_stats -> st_atime)));
+  printf("%12s ", temp);
+
+  printf("%s\n", file->d_name);
 }
 
 int main() {
-  char desired_dir[100] = "temp_dir";
-  DIR *mydir = opendir(desired_dir);;
-  struct dirent *myfile;
-  struct stat *mystat = calloc(sizeof(struct stat), 1);
-  long int total;
-  char buf[512];
+  // DIRECTORY SIZE IS SIZE OF CONTENTS (in a recursive fashion), NOT THE ACTUAL DIRECTORY FILE
+  // long long int dir_size = size_of_dir(".");
+  // printf("total size of /. : %lld B\n", dir_size);
+  // print_file_info("./mydir.c");
 
-  printf("\nStatistics for %s: \n", desired_dir);
-  while((myfile = readdir(mydir)) != NULL ) {
-    stat(myfile ->d_name, mystat);
-    total += mystat -> st_size;
+  DIR* dir = opendir(".");
+  struct dirent* current_file;
+  while ((current_file = readdir(dir)) != NULL) {
+    print_info(current_file);
   }
 
-  // formatting for bytes
-  if(total < 1000)
-  printf("Total: %ldB\n", total);
-  else if (total < 1000000)
-  printf("Total: %ld.%ldKB\n", total/1000, total%1000);
-  else if (total < 1000000000)
-  printf("Total: %ld.%ldMB\n", total/1000000, total%1000000);
-  else
-  printf("Total: %ld.%ldGB\n", total/1000000000, total%1000000000);
-
-  mydir = opendir(desired_dir);
-  myfile = readdir(mydir);
-  // printing dirs
-  printf("\ndirectories: \n");
-  while((myfile = readdir(mydir)) != NULL)
-  {
-    if(myfile -> d_type == 4) {
-      printf("  %s\n", myfile -> d_name);
-    }
-  }
-  // printing files
-  printf("\nfiles: \n");
-  mydir = opendir("./");
-  while((myfile = readdir(mydir)) != NULL) {
-    if(myfile -> d_type == 8) {
-      char filename[50] = "./";
-      strcat(filename, myfile -> d_name);
-      // printf("current name: %s\n", filename);
-      statP(filename);
-      // printf("  %s\n", myfile -> d_name);
-    }
-  }
-
-  closedir(mydir);
+  return 0;
 }
